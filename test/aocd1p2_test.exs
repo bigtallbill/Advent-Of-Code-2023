1223daebdd0f4f6a586b52611acf3b6767f63f28
Bill Nunney <bill@bigtallbill.com>
2023-12-04 07:01:49 -0500

    Add more tests for day 1 part 2

--- test/aocd1p2_test.exs
+++ test/aocd1p2_test.exs
@@ -34,6 +34,26 @@ defmodule AocD1P2Test do
     assert AOCD1P2.run() != 54159
   end
 
+  test "replace_words_with_numbers" do
+    assert AOCD1P2.replace_words_with_numbers("one212two") == "1212"
+  end
+
+  test "replace_words_with_numbers overlapping" do
+    assert AOCD1P2.replace_words_with_numbers("eightwo") == "82"
+  end
+
+  test "replace_words_with_numbers overlapping 2" do
+    assert AOCD1P2.replace_words_with_numbers("eightwothree") == "823"
+  end
+
+  test "replace_words_with_numbers mixed words" do
+    assert AOCD1P2.replace_words_with_numbers("eightwo3four") == "8234"
+  end
+
+  test "replace_words_with_numbers mixed random characters" do
+    assert AOCD1P2.replace_words_with_numbers("hheightwo3fffourfiveaa") == "82345"
+  end
+
   test "gets the right answer" do
     assert AOCD1P2.run() == 0
   end
