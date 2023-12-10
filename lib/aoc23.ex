e726aa87b722e4b0f1e1c83e4f5bc75ee649fb40
Bill Nunney <me@billnunney.dev>
2023-12-10 13:31:49 -0500

    Update main run module

--- lib/aoc23.ex
+++ lib/aoc23.ex
@@ -1,9 +1,12 @@
-defmodule Aoc23 do
+defmodule AOC23 do
   @moduledoc """
   Documentation for `Aoc23`.
   """
-  def p1 do
-    IO.puts("Part 1:" <> to_string(AOCD1P1.run()))
-    IO.puts("Part 2:" <> to_string(AOCD1P2.run()))
+  def run_all do
+    IO.puts("Day 1 Part 1: " <> to_string(AOCD1P1.run()))
+    IO.puts("Day 1 Part 2: " <> to_string(AOCD1P2.run()))
+
+    IO.puts("Day 2 Part 1: " <> to_string(AOCD2P1.run()))
+    IO.puts("Day 2 Part 2: " <> to_string(AOCD2P2.run()))
   end
 end
